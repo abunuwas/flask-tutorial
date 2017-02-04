0d5e696234d24552746e3a8d03b495c790e016ca
abunuwas <joseharoperalta@gmail.com>
2017-02-04 22:52:36 +0000

    Validate unique user nickname in requests

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length
 
+from app.models import User
+
 
 class LoginForm(FlaskForm):
     openid = StringField('openid', validators=[DataRequired()])
@@ -11,3 +13,19 @@ class LoginForm(FlaskForm):
 class EditForm(FlaskForm):
     nickname = StringField('nickname', validators=[DataRequired()])
     about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
+
+    def __init__(self, original_nickname, *args, **kwargs):
+        FlaskForm.__init__(self, *args, **kwargs)
+        self.original_nickname = original_nickname
+
+    def validate(self):
+        if not FlaskForm.validate(self):
+            return False
+        if self.nickname.data == self.original_nickname:
+            return True
+        user = User.query.filter_by(nickname=self.nickname.data).first()
+        if user:
+            self.nickname.errors.append('This nickname is already in use. '
+                                        'Please choose another one.')
+            return False
+        return True
