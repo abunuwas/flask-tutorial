1b8d31164a520aa16e4b65a597ca2ef3020a67e7
abunuwas <joseharoperalta@gmail.com>
2017-02-04 22:50:44 +0000

    Run the app with custom debug

--- app/__init__.py
+++ app/__init__.py
@@ -16,4 +16,15 @@ lm.init_app(app)
 lm.login_view = 'login'
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
+if not app.debug:
+    import logging
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
+    )
+    handler.setFormatter(formatter)
+    app.logger.addHandler(handler)
+    app.logger.setLevel(logging.DEBUG)
+    app.logger.info('microblog startup')
+
 from app import views, models
